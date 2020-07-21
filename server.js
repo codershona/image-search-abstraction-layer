6bb00ed425f7934c9c3692ccec18ab519659688b
codershona <falgunimst95@gmail.com>
2020-07-21 12:49:38 +0200

    trying to connect mongoose on server

--- server.js
+++ server.js
@@ -12,9 +12,17 @@ const mongoose = require('mongoose');
 
 const Bing = require('node-bing-api')({accKey: 'cdf83e1981944b19b8b2c6b533e8475f'});
 
+const searchTerm = require('./models/searchTerm');
+
+
 app.use(bodyParser.json());
 
 app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/searchTerms')
+
+
+
 
 // Get call with required and not required params to do a search for an image:
 
@@ -23,6 +31,23 @@ app.get('/api/imagesearch/:searchVal*', (req, res, next) => {
 	var { searchVal } = req.params;
 
 	var {offset} = req.query;
+
+
+var data = new searchTerm({
+   
+   searchVal,
+   searchDate: new Date()
+
+
+});
+
+data.save(err => {
+	if(err) {
+		res.send('Error Saving to database');
+
+	}
+	res.json(data);
+})
 
 	// console.log(searchVal);
 
