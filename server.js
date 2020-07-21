f9042249edc6662ee274f57da7d8ca7b06529b25
codershona <falgunimst95@gmail.com>
2020-07-21 11:37:26 +0200

    packages installation done

---
+++ server.js
@@ -0,0 +1,14 @@
+// Get Requirements and instatiate some of the :
+
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+
+
+
